09efb603adcd94c6a1c19e0b154097ab0c20f42e
Lienhart Woitok <lienhart.woitok@netlogix.de>
2016-03-04 19:06:14 +0100

    [TASK] Add command status

--- Tests/Unit/Command/CommandTest.php
+++ Tests/Unit/Command/CommandTest.php
@@ -6,15 +6,68 @@ namespace Netlogix\Cqrs\Tests\Unit\Command;
  */
 
 use Netlogix\Cqrs\Command\Command;
+use Netlogix\Cqrs\Command\CommandInterface;
+use Netlogix\Cqrs\Command\CommandStatusObserverInterface;
 
 /**
  * Testcase for command
  */
 class CommandTest extends \TYPO3\Flow\Tests\UnitTestCase {
 
+	/**
+	 * @var Command|\PHPUnit_Framework_MockObject_MockObject
+	 */
+	protected $command;
+
+	public function setUp() {
+		$this->command = $this->getMockBuilder(Command::class)->enableOriginalConstructor()->getMockForAbstractClass();
+	}
+
 	public function testGeneratingCommandId() {
-		/** @var Command|\PHPUnit_Framework_MockObject_MockObject $command */
-		$command = $this->getMockBuilder(Command::class)->enableOriginalConstructor()->getMockForAbstractClass();
-		$this->assertRegExp('/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/i', $command->getCommandId());
+		$this->assertRegExp('/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/i', $this->command->getCommandId());
+	}
+
+	public function testGettingDefaultStatus() {
+		$this->assertEquals(CommandInterface::STATUS_PENDING, $this->command->getStatus());
+	}
+
+	public function testUpdatingStatus() {
+		$reflectionMethod = new \ReflectionMethod($this->command, 'updateStatus');
+		$reflectionMethod->setAccessible(TRUE);
+		$reflectionMethod->invoke($this->command, CommandInterface::STATUS_SUCCESS);
+		$this->assertEquals(CommandInterface::STATUS_SUCCESS, $this->command->getStatus());
+	}
+
+	/**
+	 * @var int $status
+	 * @expectedException \InvalidArgumentException
+	 * @dataProvider invalidStatusValues
+	 */
+	public function testUpdatingStatusWithInvalidValue($status) {
+		$reflectionMethod = new \ReflectionMethod($this->command, 'updateStatus');
+		$reflectionMethod->setAccessible(TRUE);
+		$reflectionMethod->invoke($this->command, $status);
+	}
+
+	public function testStatusObserverIsCalledWhenStatusIsUpdated() {
+		/** @var CommandStatusObserverInterface|\PHPUnit_Framework_MockObject_MockObject $mockObserver */
+		$mockObserver = $this->getMockBuilder(CommandStatusObserverInterface::class)->getMockForAbstractClass();
+		$mockObserver->expects($this->once())->method('update')->with($this->command, CommandInterface::STATUS_PENDING);
+		$this->command->attachStatusObserver($mockObserver);
+
+		$reflectionMethod = new \ReflectionMethod($this->command, 'updateStatus');
+		$reflectionMethod->setAccessible(TRUE);
+		$reflectionMethod->invoke($this->command, CommandInterface::STATUS_SUCCESS);
+	}
+
+	/**
+	 * @return array
+	 */
+	public function invalidStatusValues() {
+		return [
+			[0],
+			[-1],
+			[5],
+		];
 	}
 }
