1a74dae28389a2bd4df845cdfdb2a63f3c13662d
Sascha Nowak <sascha.nowak@netlogix.de>
2017-02-03 13:47:26 +0100

    [BUGFIX] Remove ObjectManager from test

--- Tests/Unit/Log/CommandLoggerTest.php
+++ Tests/Unit/Log/CommandLoggerTest.php
@@ -5,7 +5,6 @@ namespace Netlogix\Cqrs\Tests\Unit\Log;
  * This file is part of the Netlogix.Cqrs package.
  */
 
-use Doctrine\Common\Persistence\ObjectManager;
 use Netlogix\Cqrs\Command\Command;
 use Netlogix\Cqrs\Log\CommandLogger;
 use Netlogix\Cqrs\Log\CommandLogEntryRepository;
@@ -17,12 +16,9 @@ class CommandLoggerTest extends \TYPO3\Flow\Tests\UnitTestCase {
 		$mockCommand = $this->getMockBuilder(Command::class)->getMockForAbstractClass();
 		$mockRepository = $this->getMockBuilder(CommandLogEntryRepository::class)->disableOriginalConstructor()->getMock();
 		$mockRepository->expects($this->once())->method('add');
-		$mockEntityManager = $this->getMockBuilder(ObjectManager::class)->disableOriginalConstructor()->getMock();
-		$mockEntityManager->expects($this->once())->method('flush');
 
 		$commandLogger = new CommandLogger();
 		$this->inject($commandLogger, 'commandLogEntryRepository', $mockRepository);
-		$this->inject($commandLogger, 'entityManager', $mockEntityManager);
 		$commandLogger->logCommand($mockCommand);
 	}
 }
